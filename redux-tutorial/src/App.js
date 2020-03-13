c4033cb091daa4c166796d759756e4d67db5ee14
Sudharsan Srinivasan <sudharsan_tw@twadmins-iMac.local>
2020-03-13 16:27:28 +1300

    reducer

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { value: '',postId: 2 }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.dispatch({
+      type: 'ADD_POST',
+      payload: { id: this.state.postId, title: this.state.value }
+    })
+
+    this.setState({ postId: this.state.postId + 1 })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <div>
+              <button type="submit" onClick={this.handleSubmit}>
+                Submit
+              </button>
+            </div>
+          </form>
+          <ul>
+            {this.props.posts.map(post => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { posts: state.posts }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatch
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+
+https://chriscourses.com/blog/redux
---
+++ redux-tutorial/src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux'
+
+
+
+const initialState = {
+    posts: [{id: 1, title: 'Test Post'}],
+    signUpModal :{
+        open: false
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    if(action.type === 'ADD_POST'){
+        return Object.assign({}, state,{
+            posts: state.posts.concat(action.payload)
+        })
+    }
+    return state
+}
+
+const store = createStore(reducer)
+
+export default store
